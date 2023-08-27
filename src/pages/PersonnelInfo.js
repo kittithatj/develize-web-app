addc46202b7b0646751159d71463bedd7b6f3173
Mark <attid.1987@gmail.com>
2023-08-27 18:11:32 +0700

    info personnel 50%

--- src/pages/PersonnelInfo.js
+++ src/pages/PersonnelInfo.js
@@ -1,85 +1,109 @@
-import { AvatarGroup, Box, Chip, IconButton, List, ListItemAvatar, ListItemButton, ListItemText, ThemeProvider, Tooltip, Typography, createTheme } from '@mui/material'
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { Api } from '../config/api-config';
-import ProfileAvatar from '../components/ProfileAvatar'
-import Grid from '@mui/material/Grid';
-import { Icon } from '@mui/material';
-import Button from '@mui/material/Button';
-import { Margin } from '@mui/icons-material';
+import React, { useEffect, useState } from "react";
+import { Api } from "../config/api-config";
+import Grid from "@mui/material/Grid";
+import { Avatar, Badge, Box, Typography } from "@mui/material";
+import { Person } from "@mui/icons-material";
 
-function Personnelinfo() {
+function PersonnelInfo() {
+  const [personnel, setPersonnel] = useState([]);
 
-    const [personnel, setPersonnel] = useState([])
+  const fetchPersonnelData = () => {
+    fetch(Api.url + Api.personnel_get)
+      .then((res) => res.json())
+      .then((data) => {
+        setPersonnel(data);
+      });
+  };
 
-    const fetchPersonnelData = () => {
-        fetch(Api.url + Api.personnel_get)
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                setPersonnel(data)
-            })
-    }
-    
+  useEffect(() => {
+    fetchPersonnelData();
+  }, []);
 
-    useEffect(() => {
-        fetchPersonnelData()
-    }, [])
+  const fullname = (p) => {
+    return p.firstName + " " + p.lastName;
+  };
 
-    const fullname = (p) => {
-        return p.firstName + ' ' + p.lastName;
-    }
-
-    const theme = createTheme({
-        status: {
-            danger: '#e53e3e',
-        },
-        palette: {
-            primary: {
-                main: '#0971f1',
-                darker: '#053e85',
-            },
-            success: {
-                main: '#64dd17',
-                contrastText: '#fff',
-            },
-            warning: {
-                main: '#fbc02d',
-                contrastText: '#fff',
-            }
-        },
-    });
-
-
-    return (
-    <div className='main-content'>
-      <Grid
-        container
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-      >
-        <Grid item>
+  return (
+    <div className="main-content">
+      <Grid container justifyContent="center" alignItems="stretch" spacing={2}>
+        <Grid item xs={12} md={4}>
           <Box
             sx={{
-              width: 1600,
-              height: 800,
-              backgroundColor: 'white',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              padding: '15px',
+              width: "100%",
+              backgroundColor: "white",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "15px",
             }}
           >
-            <div className='header'>
-                <Typography sx={{ mt: 1, mb: 1, fontWeight: 'bold' }} variant="h5" component="div">
-                        Personnel Management
-                    <ProfileAvatar variant='rounded' name={fullname(personnel)}/>
-                        firstName - lastName
-                </Typography>
+            <div className="header">
+              <Badge
+                overlap="circular"
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                style={{ marginLeft: "-4px" }}
+              >
+                <Avatar sx={{ width: 100, height: 100 }}>
+                  {/* รูป icon User */}
+                </Avatar>
+              </Badge>
+            </div>
+            <Typography sx={{ mt: 1, mb: 2 }} variant="h6" component="div">
+              Attid Kingkoiklang
+            </Typography>
+            <div className="header"></div>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box
+            sx={{
+              width: "100%",
+              backgroundColor: "white",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              justifyContent: "space-between",
+              padding: "15px",
+            }}
+          >
+            <div className="header">
+              <Typography
+                sx={{ mt: 1, mb: 2, color: "black" }}
+                variant="h5"
+                color="textSecondary"
+              >
+                Personnel Information
+              </Typography>
+            </div>
+            <div className="content">
+              <Typography sx={{ mt: 1,}} variant="h6" color="textSecondary">
+               <span style={{ color: "black" }}>Firstname : </span>Attid
+               <span style={{ color: "black" }}>Lastname : </span>Kingkoiklang
+              </Typography>
+              
+              <Typography sx={{ mt: 1,}} variant="h6" color="textSecondary">
+               <span style={{ color: "black" }}>Email : </span>Attid.1987@gmail.com
+              </Typography>
+
+              <Typography sx={{ mt: 1,}} variant="h6" color="textSecondary">
+               <span style={{ color: "black" }}>PhoneNumber : </span>0621234567
+              </Typography>
+
+              <Typography sx={{ mt: 1,}} variant="h6" color="textSecondary">
+               <span style={{ color: "black" }}>Division : </span>DIT630
+              </Typography>
+
+              <Typography sx={{ mt: 1,}} variant="h6" color="textSecondary">
+               <span style={{ color: "black" }}>Position : </span>Manager
+              </Typography>
+
+              <Typography sx={{ mt: 1,}} variant="h6" color="textSecondary">
+               <span style={{ color: "black" }}>EmploymentStatus : </span>immortal
+              </Typography>
             </div>
           </Box>
         </Grid>
@@ -88,4 +112,4 @@ function Personnelinfo() {
   );
 }
 
-export default Personnelinfo;
+export default PersonnelInfo;
